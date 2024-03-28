48e249f632b4e7c5b3949189d0951d900cfaee03
Luan Gregati <luan.luiz.msn@hotmail.com>
2024-03-28 13:48:56 -0300

    Update index.js
    Desafio do curso.dev

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function Home() {
-    return <h1>Clone do TabNews (em construção)</h1>;
+    return <h1>Meu amorzinho! Obrigado por me apoiar e me incentivar. Você é um dos motivos para eu estar aqui! Te amo &hearts;</h1>;
 }
