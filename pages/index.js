c42f51794cb6403302a93810e39a7dbdc7d640b0
Luan Gregati <luan.luiz.msn@hotmail.com>
2024-04-03 18:45:12 +0000

    altera a frase

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
 export default function Home() {
-    return <h1>Meu amorzinho! Obrigado por me apoiar e me incentivar. Você é um dos motivos para eu estar aqui! Te amo &hearts;</h1>;
+    return (
+        <div>
+            <h1>
+                Meu amorzinho! Obrigado por me apoiar e me incentivar. Você é um dos motivos para eu estar aqui! Te amo &hearts;
+            </h1>
+            <h2>
+                Se você leu essa mensagem, me manda um beijinho!
+            </h2>
+        </div>
+    );
 }
