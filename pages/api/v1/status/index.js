07a14939cdeaea98bd6a748fdea9db8f1e6412bb
Luan Gregati <luan.luiz.msn@hotmail.com>
2024-04-23 16:52:07 -0300

    add files `.env` and `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,16 +1,13 @@
-/**
- * Criamos uma função "status" que pode ter qualquer nome
- * Essa função recebe dois parâmetros:
- * 
- * request -> informações que são passadas lá de de fora para aqui dentro do código
- * response -> informações que o código passa para o mundo de fora
- */
-function status(request, response) {
-  // Utilizo os métodos "status" e "send" do objeto "response" para
-  // determinar qual retorno essa minha rota vai ter
-  response.status(200).json({ "mensagem": "Alunos do curso.dev são acima da média!" });
+import database from "../../../../models/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 AS sum;");
+
+  console.log(result.rows);
+
+  response
+    .status(200)
+    .json({ mensagem: "Alunos do curso.dev são acima da média!" });
 }
 
-// Export a função "status" como a função padrão da rota.
-// Dessa forma o Next.js vai saber qual função chamar quando a rota for chamada.
 export default status;
