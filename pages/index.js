39212cf2a21d87b18ccfa489504f230efe0149ee
Luan Gregati <luan.luiz.msn@hotmail.com>
2024-04-29 18:03:02 -0300

    changes to construction page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1>
-        Meu amorzinho! Obrigado por me apoiar e me incentivar. Você é um dos
-        motivos para eu estar aqui! Te amo &hearts;
-      </h1>
-      <h2>Se você leu essa mensagem, me manda um beijinho!</h2>
+      <h1>Em construção</h1>
+      <p>Não há muito para se ver aqui...</p>
     </div>
   );
 }
