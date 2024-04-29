5e4a0c6be674d3f7f22e703ec0bec4c57c987da2
Luan Gregati <luan.luiz.msn@hotmail.com>
2024-04-29 18:04:24 -0300

    tests improvement

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,13 +1,29 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
+  const updated_at = new Date().toISOString();
+  const postgres_version = await database.query("SHOW server_version;");
+  const postgres_max_connections = await database.query(
+    "SHOW max_connections;",
+  );
+  const postgres_active_connections = await database.query({
+    text: "SELECT COUNT(*)::int AS active_connections FROM pg_stat_activity WHERE datname = $1",
+    values: [process.env.POSTGRES_DB],
+  });
 
-  console.log(result.rows);
-
-  response
-    .status(200)
-    .json({ mensagem: "Alunos do curso.dev são acima da média!" });
+  response.status(200).json({
+    updated_at,
+    dependencies: {
+      database: {
+        version: postgres_version.rows[0].server_version,
+        max_connections: parseInt(
+          postgres_max_connections.rows[0].max_connections,
+        ),
+        opened_connections:
+          postgres_active_connections.rows[0].active_connections,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,34 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-
   expect(response.status).toBe(200);
+});
+
+test('/api/v1/status "updated_at" value should be defined and shoud be an ISO String', async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBeDefined();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+});
+
+test("PostgreSQL version should be 16.0", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+});
+
+test("PostgreSQL max connection should be 100", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+});
+
+test("PostgreSQL active connections in local connection should be 1", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
 });
