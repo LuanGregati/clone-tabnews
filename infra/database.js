15a024fe4d6f626c2b3eb55c7f935bebc28fc48c
Luan Gregati <luan.luiz.msn@hotmail.com>
2024-04-30 13:26:39 -0300

    makes `database.js` `client.connect()` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -9,13 +9,21 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
+  console.log("Credenciais do Postgres:", {
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
-  } catch (err) {
+  } catch (error) {
     console.error(err);
+    throw error;
   } finally {
     await client.end();
   }
