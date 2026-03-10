0619bf24afe4a1c5f66fee6f390a1b4cca2dab96
Luan Gregati <luan.luiz.msn@hotmail.com>
2026-03-10 12:41:09 -0300

    D20A5 finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,13 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
 
   expect(response.status).toBe(200);
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBe(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toBe("16.12");
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
 });
