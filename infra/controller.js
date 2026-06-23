efb6dd886d9c51e81c073bc397a225197554a48c
Luan Gregati <luan.luiz.msn@hotmail.com>
2026-06-23 18:09:21 -0300

    feat: d46a6 invalidate session cookie on unauthorized error

--- infra/controller.js
+++ infra/controller.js
@@ -14,11 +14,13 @@ function onNoMatchHandler(_, response) {
 }
 
 function onErrorHandler(error, _, response) {
-  if (
-    error instanceof ValidationError ||
-    error instanceof NotFoundError ||
-    error instanceof UnauthorizedError
-  ) {
+  if (error instanceof ValidationError || error instanceof NotFoundError) {
+    return response.status(error.statusCode).json(error);
+  }
+
+  if (error instanceof UnauthorizedError) {
+    clearSessionCookie(response);
+
     return response.status(error.statusCode).json(error);
   }
 
